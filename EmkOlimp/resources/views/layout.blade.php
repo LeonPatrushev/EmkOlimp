bb26988f1ad4bcc7a90aa17104e702ca589f37aa
Leon Patrushev <leonpatrushev@yandex.ru>
2022-05-13 16:46:54 +0500

    13052022-add-style

--- EmkOlimp/resources/views/layout.blade.php
+++ EmkOlimp/resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EmkOlimp</title>
     <link rel="stylesheet" href="{{URL::asset('storage/assets/css/main.css')}}">
 </head>
 <body>
@@ -12,7 +12,9 @@
     <header>
         <div class="header-upper-block-grid">
             <div class="header-upper-block">
-                <img class="logo-img" src="{{URL::asset('storage/assets/img/LogoEMK.png')}}" alt="LogoEMK">
+                <a class="logo-link" href="">
+                    <img class="logo-img" src="{{URL::asset('storage/assets/img/LogoEMK.png')}}" alt="LogoEMK">
+                </a>
                 <H1>Международная дистанционная олимпиада «Проектирование и разработка баз данных»</H1>
             </div>
         </div>
